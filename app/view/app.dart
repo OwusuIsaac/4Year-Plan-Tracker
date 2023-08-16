d2c6af6f179624288498415b5934878b91827315
OwusuIsaac <105105471+OwusuIsaac@users.noreply.github.com>
2023-08-16 07:34:58 +0000

    Update app.dart

--- app/view/app.dart
+++ app/view/app.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:activities_repository/activities_repository.dart';
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
